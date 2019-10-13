a12d676d98c0533f14876a3e12d0c617963d9d9c
123dhanush <gunthadanush111@gmail.com>
2019-10-13 23:09:22 +0530

    2nd commit

---
+++ notesapp/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.contrib import admin
+from django.conf.urls.static import static
+from daproject import settings
+from . import views
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('troute/',views.troute,name="troute"),
+    path('sroute/',views.sroute,name="sroute"),
+    path('troute/tlogin/upload/',views.upload,name="upload"),
+    path('troute/tlogin/',views.tlogin,name="tlogin"),
+    path('sroute/slogin/',views.slogin,name="slogin"),
+    path('troute/tregister/',views.tregister,name="tregister"),
+    path('sroute/sregister/',views.sregister,name="sregister"),
+    path('troute/tlogin/upload_success/',views.upload_success,name="upload_success"),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
